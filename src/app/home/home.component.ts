2c2ae6566484280e716924d37ca7e6b78964d37f
Hanzhang ma <583886027@qq.com>
2023-05-14 18:17:29 +0200

    176 Use pipe to preprocess the data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {interval, Observable, Subscription} from "rxjs";
+import {filter, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -19,17 +20,21 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(()=>{
         observer.next(count);
-        if(count === 2){
+        if(count === 5){
           observer.complete();
         }
-        if(count > 3){
+        if(count >= 3){
           observer.error(new Error('Count is greater than 3'));
         }
         count++;
       },1000)
     });
-    this.firstObsSubscription = cunstomIntervalObservable.subscribe(data => {
-      console.log(data);
+    this.firstObsSubscription = cunstomIntervalObservable.pipe(filter(data=> {
+      return data > 0;
+    }),map((data: number) => {
+      return "Round: " + (data + 1);
+    })).subscribe(datas => {
+      console.log(datas);
     }, error => {
       console.log(error);
       alert(error);
